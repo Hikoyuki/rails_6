4abc6cf7af7f8a56c583b6cd77b160a0d1782a35
Hikoyuki <hikoyuki.sakai@gmail.com>
2020-06-14 13:08:26 +0900

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+User.create!(name: "酒井", age: 22)
+User.create!(name: "斎藤", age: 25)
+User.create!(name: "山", age: 31)
+User.create!(name: "山田", age: 32)
+User.create!(name: "池辺", age: 33)
+User.create!(name: "溝上", age: 35)
